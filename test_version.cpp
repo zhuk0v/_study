78895618e125ee3de82357d50fddacd9f8efe025
Sergey <serfer@area.zov>
2024-03-04 22:29:52 +0300

    Move to google_test

--- test_version.cpp
+++ test_version.cpp
@@ -1,13 +1,8 @@
-#define BOOST_TEST_MODULE test_version
+#include <gtest/gtest.h>
 
 #include "lib.h"
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version) {
-	BOOST_CHECK(version() > 0);
+TEST(HelloWorldTests, TestVersion) {
+  // Expect that the version number is greater than 0
+  EXPECT_GT(version(), 0);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
