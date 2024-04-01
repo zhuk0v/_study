f8feaa37456909f1881c33cb5134912176ad147a
Sergey <serfer@area.zov>
2024-04-01 11:42:10 +0300

    The "print_ip" function is implemented using SFINAE

--- main.cpp
+++ main.cpp
@@ -1,122 +1,86 @@
 #include <cstdlib>
+#include <type_traits>
+
+#include <string>
 #include <vector>
-#include <map>
+#include <list>
+#include <tuple>
 
 #include <iostream>
-
-#include "cust_alloc.hpp"
-#include "cust_container.hpp"
+#include <algorithm>
 
 #define UNUSE(x) (void)(x)
 
-
-
-template <typename Key, typename Tp>
-using map_1 = std::map<Key, Tp>;
-
-template <typename T>
-using vct_1 = std::vector<T>;
-
-template <typename Key, typename Tp, std::size_t N, bool Fix>
-using resource_map = custom::AllocatorResource<std::pair<const Key, Tp>, N, Fix>;
-
-template <typename Key, typename Tp>
-using map_2 = std::pmr::map<Key, Tp>;
-
-template <typename T, std::size_t N, bool Fix>
-using resource_vct = custom::AllocatorResource<T, N, Fix>;
+// Print int8_t, int16_t, int32_t, int64_t
+template<typename T>
+void print_byte(T& val, int pos) {
+	std::cout << (((std::make_unsigned_t<T>)val >> (pos * 8)) & (std::make_unsigned_t<T>)0xFF);
+}
 
 template <typename T>
-using vct_2 = std::pmr::vector<T>;
+struct cond_0 { static constexpr bool value = !std::is_class<T>::value; };
 
-template <typename T, std::size_t N, bool Fix>
-using resource_cnt = resource_vct<T, N, Fix>;
-
-template <typename T>
-using cnt = custom::Container<T>;
-
-
-auto fill_map = [](auto& l_map) {
-	// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
-	l_map[0] = 1;
-	for (std::size_t i = 1; i < 10; i++) {
-		l_map[i] = l_map[i - 1] * i;
+template<typename T, std::size_t n_byte = sizeof(T), typename Fake = typename std::enable_if<cond_0<T>::value>::type>
+void print_ip(T val) {
+	for (std::size_t i = n_byte - 1; i >= 1; i--) {
+		print_byte(val, i);
+		std::cout << ".";
 	}
-};
+	print_byte(val, 0);
+	std::cout << std::endl;
+}
 
-auto print_map = [](const auto& l_map) {
-	// вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
-	for (const auto& pair : l_map) {
-		std::cout << pair.first << " " << pair.second << std::endl;
+// std::string
+template<typename T, typename std::enable_if<
+	(!std::is_integral<T>::value) &&
+	(std::is_same<T, std::basic_string<typename T::value_type, typename T::traits_type, typename T::allocator_type>>::value)
+	>::type* = nullptr>
+void print_ip(T val) {
+	std::cout << val << std::endl;
+}
+
+// std::vector
+// std::list
+template<typename T, typename std::enable_if<
+	!std::is_integral<T>::value &&
+	(std::is_same<T, std::vector<typename T::value_type, typename T::allocator_type>>::value ||
+	std::is_same<T, std::list<typename T::value_type, typename T::allocator_type>>::value)
+	>::type* = nullptr>
+void print_ip(T val) {
+	auto it = val.begin();
+	for (std::size_t i = 0; i < val.size() - 1; i++, it++) {
+		std::cout << *it << '.';
 	}
-};
+	std::cout << *it << std::endl;
+}
 
-auto fill_vector = [](auto& l_vect) {
-	// заполнение 10 элементами от 0 до 9
-	for (std::size_t i = 0; i < 10; i++) {
-		l_vect.push_back(i);
-	}
-};
 
-auto print_vector = [](const auto& l_vect) {
-	// вывод на экран всех значений, хранящихся в контейнере
-	for (const auto& el : l_vect) {
-		std::cout << el << std::endl;
-	}
-};
+// std::tuple
+template<class TupleType, std::size_t... I>
+void print_tuple_values(const TupleType& val, std::index_sequence<I...>) {
+    (..., (std::cout << std::get<I>(val) << "."));
+}
 
+template<typename T, std::size_t n_el = std::tuple_size<T>::value, typename std::enable_if<
+	!std::is_integral<T>::value
+	>::type* = nullptr>
+void print_ip(T val) {
+	print_tuple_values(val, std::make_index_sequence<n_el-1>());
+	std::cout << std::get<n_el-1>(val) << std::endl;
+}
 
 int main(int argc, char const* argv []) {
 	UNUSE(argc);
 	UNUSE(argv);
 
-	const std::size_t num_elements = 10;
-	const bool fix_my_mem = false;
-
-	// создание экземпляра std::map<int, int>
-	map_1<int, int> factorials;
-	// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
-	fill_map(factorials);
-
-	// выделение памяти
-	resource_map<int, int, num_elements, fix_my_mem> map_2_res;
-	// создание экземпляра std::map<int, int> с новым аллокатором, ограниченным 10 элементами
-	map_2<int, int> factorials_with_cust_alloc(&map_2_res);
-	// заполнение 10 элементами, где ключ - это число от 0 до 9, а значение - факториал ключа
-	fill_map(factorials_with_cust_alloc);
-
-	// вывод на экран всех значений (ключ и значение разделены пробелом) хранящихся в контейнере
-	print_map(factorials);
-	print_map(factorials_with_cust_alloc);
-
-	// создание экземпляра std::vector<int>
-	vct_1<int> seq;
-	// заполнение 10 элементами от 0 до 9
-	fill_vector(seq);
-
-	// выделение памяти
-	resource_vct<int, num_elements, fix_my_mem> vct_2_res;
-	// создание экземпляра std::vector<int> с новым аллокатором, ограниченным 10 элементами
-	vct_2<int> seq_with_cust_alloc(&vct_2_res);
-	// заполнение 10 элементами от 0 до 9
-	fill_vector(seq_with_cust_alloc);
-
-	// вывод на экран всех значений, хранящихся в контейнере
-	print_vector(seq);
-	print_vector(seq_with_cust_alloc);
-
-	// выделение памяти
-	resource_cnt<cnt<int>::value_type, num_elements, fix_my_mem> cnt_res;
-	// создание экземпляра своего контейнера для хранения значений типа int
-	cnt<int> seq_with_cust_cont_and_cust_alloc(&cnt_res);
-	// заполнение 10 элементами от 0 до 9
-	for (std::size_t i = 0; i < 10; i++) {
-		seq_with_cust_cont_and_cust_alloc.append(i);
-	}
-	// вывод на экран всех значений, хранящихся в контейнере
-	for (auto& el : seq_with_cust_cont_and_cust_alloc) {
-		std::cout << el << std::endl;
-	}
+	print_ip(int8_t{ -1 }); // 255
+	print_ip(int16_t{ 0 }); // 0.0
+	print_ip(int32_t{ 2130706433 }); // 127.0.0.1
+	print_ip(int64_t{ 8875824491850138409 });// 123.45.67.89.101.112.131.41
+	print_ip(std::string{ "Hello, World!" }); // Hello, World!
+	print_ip(std::vector<int>{100, 200, 300, 400}); // 100.200.300.400
+	print_ip(std::list<short>{400, 300, 200, 100}); // 400.300.200.100
+	print_ip(std::make_tuple(123, 456, 789, 0)); // 123.456.789.0
 
 	return EXIT_SUCCESS;
 }
