5f8a1b7e1afc706c6d0821fa48aae2bd3bd2c647
Sergey <serfer@area.zov>
2024-05-17 10:31:39 +0300

    Initialize bulk

--- main.cpp
+++ main.cpp
@@ -1,34 +1,127 @@
 #include <cassert>
 #include <iostream>
 
-#include "matr.hpp"
+#include <string>
+#include <queue>
 
 #define UNUSE(x) (void)(x)
+
+class Command {
+private:
+	std::string val;
+public:
+	Command() = default;
+	~Command() = default;
+
+	bool IsOpenBlock() const {
+		return val[0] == '{' && val.size() == 1;
+	}
+
+	bool IsEndBlock() const {
+		return val[0] == '}' && val.size() == 1;
+	}
+
+	bool IsEnd() const {
+		return val == "EOF" && val.size() == 3;
+	}
+
+	friend std::istream& operator >> (std::istream& in, Command& cmd);
+	friend std::ostream& operator << (std::ostream& out, Command& cmd);
+};
+
+std::istream& operator >> (std::istream& in, Command& cmd) {
+	in >> cmd.val;
+	return in;
+}
+
+std::ostream& operator << (std::ostream& out, Command& cmd) {
+	out << cmd.val;
+	return out;
+}
+
+class CmdProc {
+private:
+	std::size_t n_inp_cmd;
+	std::queue<Command> commands;
+public:
+	CmdProc(std::size_t n) : n_inp_cmd(n) {}
+	~CmdProc() {}
+
+	void poll(std::istream& in) {
+		Command cmd{};
+
+		for (auto i = 0; ; i++) {
+
+			if (commands.size() == n_inp_cmd) {
+				flush();
+			}
+
+			in >> cmd;
+
+			if (cmd.IsOpenBlock()) {
+				if (commands.size() != 0) {
+					flush();
+				}
+				if (poll_dyn_block(in, cmd)) {
+					flush();
+					continue;
+				}
+				break;
+			}
+
+			if (cmd.IsEnd()) {
+				flush();
+				break;
+			}
+
+			commands.push(cmd);
+		}
+	}
+
+private:
+
+	bool poll_dyn_block(std::istream& in, Command& cmd) {
+		for (auto i = 0; ; i++) {
+			in >> cmd;
+
+			if (cmd.IsOpenBlock()) {
+				if (poll_dyn_block(in, cmd)) {
+					continue;
+				}
+				return false;
+			}
+
+			if (cmd.IsEndBlock()) {
+				return true;
+			}
+
+			if (cmd.IsEnd()) {
+				return false;
+			}
+
+			commands.push(cmd);
+		}
+	}
+
+	void flush() {
+		std::cout << "bulk: ";
+		while (commands.size() > 0) {
+			std::cout << commands.front() << ',';
+			commands.pop();
+		}
+		std::cout << '\b' << ' ' << std::endl;
+
+	};
+};
 
 
 int main(int argc, char const* argv []) {
 	UNUSE(argc);
 	UNUSE(argv);
 
-	// // бесконечная матрица int заполнена значениями -1
-	matr::Matrix<int, 2> matrix(-1);
-	assert(matrix.size() == 0); // все ячейки свободны
-	auto a = matrix[0][0];
-	assert(a == -1);
-	assert(matrix.size() == 0);
-	matrix[100][100] = 314;
-	assert(matrix[100][100] == 314);
-	assert(matrix.size() == 1);
-	// выведется одна строка
-	// 100100314
-	for (auto c : matrix) {
-		int x;
-		int y;
-		int v;
-		std::tie(x, y, v) = c;
-		std::cout << x << y << v << std::endl;
-	}
-	((matrix[100][100] = 314) = 0) = 217;
+	CmdProc cmd_proc{ static_cast<std::size_t>(std::stoll(std::string(argv[1]))) };
+
+	cmd_proc.poll(std::cin);
 
 	return EXIT_SUCCESS;
 }
